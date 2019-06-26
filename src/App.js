5a29a9f80285e12b3a0416c9633a093817d32981
rafaelvicio <rafaelvicio@icloud.com>
2019-06-26 00:58:47 -0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { withFormik } from 'formik';
+import Select from 'react-select';
+import * as Yup from 'yup';
+
+const sexs = [
+  { value: 'M', label: 'Masculino'},
+  { value: 'F', label: ' Feminino'}
+]
+
+function App({ handleSubmit, erros, values, handleChange, setFieldValue }) {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="name" value={values.name} onChange={handleChange} />
+      {/* { !!erros.name && <span>{erros.name}</span>} */}
+      <input type="text" name="email" value={values.email} onChange={handleChange} />
+      <Select placeholder="Escolha o sexo..." options={sexs} value={values.sex} onChange={value => setFieldValue('sex', value)} />
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
-export default App;
+export default withFormik({
+  mapPropsToValues: (props) => ({
+  name: 'abc',
+  email: '',
+  sex: { value: 'M', label: ' Masculino'}
+  }),
+  validateOnChange: false,
+  validateOnBlur: false,
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required('Campo obrigatório'),
+    email: Yup.string().required('Campo obrigatório')
+  }),
+  handleSubmit: (values, {props}) => {
+    const payload = {
+      ...values,
+      sex: values.sex.value
+    }
+    console.log('-->', payload)
+  },
+})(App);
